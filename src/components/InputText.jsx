650d559ebdd0f916afaeb285e9c8e1c2b2bd4bf5
LuizBlume <luiz.fernando.251107@gmail.com>
2024-07-16 15:26:27 -0300

    Test: keyword butotn enter.

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,8 +1,14 @@
 // src/components/InputText.jsx
 import React from 'react';
 const Input = ({ value, onChange }) => {
+  // var input = document.getElementById('input');
+  // input.addEventListener('keypress', function(e) {
+  //   if (KeyboardEvent === "Enter") {
+      
+  //   }
+  // })
   return (
-    <input type="text" value={value} onChange={(e) => onChange(e.target.value)} placeholder="Enter a task"
+    <input id='input' type="text" value={value} onChange={(e) => onChange(e.target.value)} placeholder="Enter a task"
     />
   );
 };
